2baf56019ad4f2091fff6f25639c724cb77d2eae
Sean Miller <sean@seanmiller.ca>
2023-09-22 19:10:10 -0400

    Add simplecov (locally)
    Adding simplecov revealed that the lib/reformat.rb file, as a script
    using optparse, doesn't readily fall into code coverage.  We were
    still testing the functionality, by issuing command-line calls in the spec,
    so we knew that it worked but couldn't see in reports that it was covered.
    
    Adding simplecov influenced the change to move the non-optparse code
    from the script into lib/file_interface.rb, which was a class and could then
    be tested and code-coveraged exhaustively.
    
    And we've still got the reformat_spec, renamed here cli_spec, which checks that
    the bits in the optparse script file still work as expected, even if we
    can't claim coverage for them.

---
+++ lib/file_interface.rb
@@ -0,0 +1,45 @@
+require_relative 'formatter'
+
+class FileInterface
+
+  attr_accessor :errors
+
+  def initialize(options:, stderr: $stderr)
+    @options = options
+    @stderr = stderr
+  end
+
+  def validate
+    @errors = []
+
+    if !@options.has_key?(:in)
+      @errors << "-i FILE option for input file required"
+    end
+    if !@options.has_key?(:out)
+      @errors << "-o FILE option for output file required"
+    end
+
+    if @options.include?(:in) && !File.file?(@options[:in])
+      @errors << "input file must exist"
+    end
+    if @options.include?(:out) && File.file?(@options[:out])
+      @errors << "output file must not exist"
+    end
+  end
+
+  def process
+    validate
+    if @errors.any?
+      @stderr.puts @errors
+      exit 1
+    end
+
+    input = File.open(@options[:in]).readlines.join("")
+
+    output = Formatter.new.format(input)
+
+    File.open(@options[:out], "w") do |f|
+      f.write(output)
+    end
+  end
+end
---
+++ spec/file_interface_spec.rb
@@ -0,0 +1,90 @@
+require 'file_interface'
+
+RSpec.describe FileInterface do
+
+  before do
+    FileUtils.rm_r("spec/tmp_files") if Dir.exist?("spec/tmp_files")
+    FileUtils.mkdir("spec/tmp_files")
+  end
+
+  describe "unhappy paths" do
+    describe "multiple errors can appear" do
+      let(:ui) { FileInterface.new(options: {}) }
+      it "has both expected error messages" do
+        ui.validate
+        expect(ui.errors).to match_array(["-i FILE option for input file required",
+                                          "-o FILE option for output file required"])
+      end
+    end
+
+    describe "no --in argument" do
+      let(:ui) { FileInterface.new(options: {out: "spec/tmp_files/actual.txt"}) }
+
+      it "has expected error message" do
+        ui.validate
+        expect(ui.errors).to match_array(["-i FILE option for input file required"])
+      end
+
+      it "exits" do
+        expect { ui.process }.to raise_error SystemExit
+      end
+    end
+
+    describe "no --out argument" do
+      let(:ui) { FileInterface.new(options: {in: "spec/example_files/input.txt"}) }
+
+      it "has expected error message" do
+        ui.validate
+        expect(ui.errors).to match_array(["-o FILE option for output file required"])
+      end
+
+      it "exits" do
+        expect { ui.process }.to raise_error SystemExit
+      end
+    end
+
+    describe "input file doesn't exist (and it should)" do
+      let(:ui) { FileInterface.new(options: {in: "spec/example_files/macavitys_not_there.txt", out: "spec/tmp_files/actual.txt"}) }
+
+      it "has expected error message" do
+        ui.validate
+        expect(ui.errors).to match_array(["input file must exist"])
+      end
+
+      it "exits" do
+        expect { ui.process }.to raise_error SystemExit
+      end
+    end
+
+    describe "output file already exists (and it shouldn't)" do
+      let(:ui) { FileInterface.new(options: {in: "spec/example_files/input.txt", out: "spec/example_files/input.txt"}) }
+
+      it "has expected error message" do
+        ui.validate
+        expect(ui.errors).to match_array(["output file must not exist"])
+      end
+
+      it "exits" do
+        expect { ui.process }.to raise_error SystemExit
+      end
+    end
+  end
+
+  it "happy path" do
+    input_file_path = "spec/example_files/input.txt"
+    expected_output_file_path = "spec/example_files/expected_output.txt"
+    output_file_path = "spec/tmp_files/actual.txt"
+
+    options = {
+      in: input_file_path,
+      out: output_file_path
+    }
+
+    FileInterface.new(options: options).process
+
+    actual_lines = File.open(output_file_path).readlines
+    expected_lines = File.open(expected_output_file_path).readlines
+
+    expect(actual_lines).to match_array(expected_lines)
+  end
+end
