fce7bc279fdc28e87d39adb364fc51e791aa3497
Sean Miller <sean@seanmiller.ca>
2023-09-21 22:36:31 -0400

    Format with stanzas and blank lines between

--- lib/formatter.rb
+++ lib/formatter.rb
@@ -3,19 +3,42 @@ class Formatter
   def format(input)
     parts = input.split("\n\n")
 
-    lines_orig = parts[0].split("\n")
-    max_length_orig = lines_orig.map(&:size).max
+    original, translation = parts.partition.with_index { |_,i| i.even? }
 
-    lines_transl = parts[1].split("\n")
-    max_length_transl = lines_transl.map(&:size).max
+    @line_length_original = max_length(original)
+    @line_length_translation = max_length(translation)
 
     output_lines = []
-    output_lines << "|-#{'-'*max_length_orig}-+-#{'-'*max_length_transl}-|"
+    output_lines << header_line
 
-    lines_orig.zip(lines_transl).each do |pair|
-      output_lines << "| #{pair[0].ljust(max_length_orig, ' ')} | #{pair[1].ljust(max_length_transl, ' ')} |"
+    output_lines << stanza(original.first, translation.first)
+
+    more_stanzas_original = original.drop(1)
+    more_stanzas_translation = translation.drop(1)
+
+    more_stanzas_original.zip(more_stanzas_translation).each do |pair|
+      output_lines << stanza_break_line
+      output_lines << stanza(pair[0], pair[1])
     end
 
     "#{output_lines.join("\n")}\n"
+  end
+
+  def max_length(stanzas)
+    stanzas.map { |s| s.split("\n") }.flatten.map(&:size).max
+  end
+
+  def header_line
+    "|-#{'-'*@line_length_original}-+-#{'-'*@line_length_translation}-|"
+  end
+
+  def stanza_break_line
+    "| #{' '*@line_length_original} | #{' '*@line_length_translation} |"
+  end
+
+  def stanza(original, translation)
+    original.split("\n").zip(translation.split("\n")).map do |pair|
+      "| #{pair[0].ljust(@line_length_original, ' ')} | #{pair[1].ljust(@line_length_translation, ' ')} |"
+    end
   end
 end
--- spec/formatter_spec.rb
+++ spec/formatter_spec.rb
@@ -2,7 +2,7 @@ require_relative '../lib/formatter'
 
 RSpec.describe Formatter do
 
-  it "does alternate" do
+  it "does alternate without stanzas" do
     input = <<~DOC
       original-1
       original-2 eheu
@@ -15,6 +15,38 @@ RSpec.describe Formatter do
       |-----------------+-----------------------|
       | original-1      | translation-1         |
       | original-2 eheu | translation-2 oh dear |
+    DOC
+
+    formatter = Formatter.new
+
+    expect(formatter.format(input)).to eq(expected_output)
+  end
+
+  it "does alternate with stanzas" do
+    input = <<~DOC
+      original-st1-1
+      original-st1-2 eheu
+
+      translation-st1-1
+      translation-st1-2 oh dear
+
+      original-st2-3
+      original-st2-4
+      original-st2-5
+
+      translation-st2-3
+      translation-st2-4
+      translation-st2-5
+    DOC
+
+    expected_output = <<~DOC
+      |---------------------+---------------------------|
+      | original-st1-1      | translation-st1-1         |
+      | original-st1-2 eheu | translation-st1-2 oh dear |
+      |                     |                           |
+      | original-st2-3      | translation-st2-3         |
+      | original-st2-4      | translation-st2-4         |
+      | original-st2-5      | translation-st2-5         |
     DOC
 
     formatter = Formatter.new
