724c6f997d82a1e41418e5c1ddad7c66312041e4
Sean Miller <sean@seanmiller.ca>
2023-09-24 13:57:24 -0400

    Comment out all specs to provoke coverage failure

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -3,33 +3,33 @@ require 'open3'
 
 RSpec.describe "cli for FileInterface" do
 
-  before do
-    FileUtils.rm_r("spec/tmp_files") if Dir.exist?("spec/tmp_files")
-    FileUtils.mkdir("spec/tmp_files")
-  end
+  # before do
+  #   FileUtils.rm_r("spec/tmp_files") if Dir.exist?("spec/tmp_files")
+  #   FileUtils.mkdir("spec/tmp_files")
+  # end
 
-  it "displays help if no options selected" do
-    stdout, _, _ = Open3.capture3("./reformat")
-    expect(stdout).to include("Reformats dual-language texts")
-  end
+  # it "displays help if no options selected" do
+  #   stdout, _, _ = Open3.capture3("./reformat")
+  #   expect(stdout).to include("Reformats dual-language texts")
+  # end
 
-  it "invalid path: returns error message" do
-    input_file_path = "spec/example_files/macavitys_not_there.txt"
-    _, stderr, _ = Open3.capture3("./reformat alternating --in #{input_file_path}")
-    expect(stderr).to include("input file must exist")
-    expect(stderr).to include("-o FILE option for output file required")
-  end
+  # it "invalid path: returns error message" do
+  #   input_file_path = "spec/example_files/macavitys_not_there.txt"
+  #   _, stderr, _ = Open3.capture3("./reformat alternating --in #{input_file_path}")
+  #   expect(stderr).to include("input file must exist")
+  #   expect(stderr).to include("-o FILE option for output file required")
+  # end
 
-  it "happy path: writes correct output file" do
-    input_file_path = "spec/example_files/input.txt"
-    expected_output_file_path = "spec/example_files/expected_output.txt"
-    output_file_path = "spec/tmp_files/actual.txt"
+  # it "happy path: writes correct output file" do
+  #   input_file_path = "spec/example_files/input.txt"
+  #   expected_output_file_path = "spec/example_files/expected_output.txt"
+  #   output_file_path = "spec/tmp_files/actual.txt"
 
-    Open3.capture3("./reformat alternating --in #{input_file_path} --out #{output_file_path}")
+  #   Open3.capture3("./reformat alternating --in #{input_file_path} --out #{output_file_path}")
 
-    actual_lines = File.open(output_file_path).readlines
-    expected_lines = File.open(expected_output_file_path).readlines
+  #   actual_lines = File.open(output_file_path).readlines
+  #   expected_lines = File.open(expected_output_file_path).readlines
 
-    expect(actual_lines).to match_array(expected_lines)
-  end
+  #   expect(actual_lines).to match_array(expected_lines)
+  # end
 end
--- spec/formatter_spec.rb
+++ spec/formatter_spec.rb
@@ -2,55 +2,55 @@ require 'formatter'
 
 RSpec.describe Formatter do
 
-  it "does alternate without stanzas" do
-    input = <<~DOC
-      original-1
-      original-2 eheu
+  # it "does alternate without stanzas" do
+  #   input = <<~DOC
+  #     original-1
+  #     original-2 eheu
 
-      translation-1
-      translation-2 oh dear
-    DOC
+  #     translation-1
+  #     translation-2 oh dear
+  #   DOC
 
-    expected_output = <<~DOC
-      |-----------------+-----------------------|
-      | original-1      | translation-1         |
-      | original-2 eheu | translation-2 oh dear |
-    DOC
+  #   expected_output = <<~DOC
+  #     |-----------------+-----------------------|
+  #     | original-1      | translation-1         |
+  #     | original-2 eheu | translation-2 oh dear |
+  #   DOC
 
-    formatter = Formatter.new
+  #   formatter = Formatter.new
 
-    expect(formatter.format(input)).to eq(expected_output)
-  end
+  #   expect(formatter.format(input)).to eq(expected_output)
+  # end
 
-  it "does alternate with stanzas" do
-    input = <<~DOC
-      original-st1-1
-      original-st1-2 eheu
+  # it "does alternate with stanzas" do
+  #   input = <<~DOC
+  #     original-st1-1
+  #     original-st1-2 eheu
 
-      translation-st1-1
-      translation-st1-2 oh dear
+  #     translation-st1-1
+  #     translation-st1-2 oh dear
 
-      original-st2-3
-      original-st2-4
-      original-st2-5
+  #     original-st2-3
+  #     original-st2-4
+  #     original-st2-5
 
-      translation-st2-3
-      translation-st2-4
-      translation-st2-5
-    DOC
+  #     translation-st2-3
+  #     translation-st2-4
+  #     translation-st2-5
+  #   DOC
 
-    expected_output = <<~DOC
-      |---------------------+---------------------------|
-      | original-st1-1      | translation-st1-1         |
-      | original-st1-2 eheu | translation-st1-2 oh dear |
-      |                     |                           |
-      | original-st2-3      | translation-st2-3         |
-      | original-st2-4      | translation-st2-4         |
-      | original-st2-5      | translation-st2-5         |
-    DOC
+  #   expected_output = <<~DOC
+  #     |---------------------+---------------------------|
+  #     | original-st1-1      | translation-st1-1         |
+  #     | original-st1-2 eheu | translation-st1-2 oh dear |
+  #     |                     |                           |
+  #     | original-st2-3      | translation-st2-3         |
+  #     | original-st2-4      | translation-st2-4         |
+  #     | original-st2-5      | translation-st2-5         |
+  #   DOC
 
-    formatter = Formatter.new
+  #   formatter = Formatter.new
 
-    expect(formatter.format(input)).to eq(expected_output)
-  end
+  #   expect(formatter.format(input)).to eq(expected_output)
+  # end
 end
