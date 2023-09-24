d3c56288fc049cd3e5e6f31a1cd78f75c58a9296
Sean Miller <sean@seanmiller.ca>
2023-09-24 19:26:33 -0400

    Refactor to bring more code under code coverage

--- lib/reformat.rb
+++ lib/reformat.rb
@@ -2,52 +2,7 @@
 
 require 'optparse'
 
-require_relative 'formatter'
-
-def alternating_check_params(options)
-  errors = []
-
-  if !options.has_key?(:in)
-    errors << "-i FILE option for input file required"
-  end
-  if !options.has_key?(:out)
-    errors << "-o FILE option for output file required"
-  end
-
-  if errors.any?
-    $stderr.puts errors
-    exit 1
-  end
-
-  if options.include?(:in) && !File.file?(options[:in])
-    errors << "input file must exist"
-  end
-  if options.include?(:out) && File.file?(options[:out])
-    errors << "output file must not exist"
-  end
-
-  if errors.any?
-    $stderr.puts errors
-    exit 1
-  end
-end
-
-def alternating_format(options)
-  input = File.open(options[:in]).readlines.join("")
-
-  formatter = Formatter.new
-
-  output = formatter.format(input)
-
-  File.open(options[:out], "w") do |f|
-    f.write(output)
-  end
-end
-
-def process_alternating(options)
-  alternating_check_params(options)
-  alternating_format(options)
-end
+require_relative 'processor'
 
 options = {}
 
@@ -83,5 +38,5 @@ if (variant.nil? && options.empty?) || variant == "help"
 end
 
 if variant == "alternating"
-  process_alternating(options)
+  Processor.new(options: options).process
 end
